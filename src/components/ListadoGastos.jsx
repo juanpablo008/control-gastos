d575255fd7cfa983038d128927986bfb392bed1e
Juan Pablo Bedoya Mosquera <bedojuanpablo@gmail.com>
2022-03-13 21:57:31 -0500

    13/03/2022 - 3

--- src/components/ListadoGastos.jsx
+++ src/components/ListadoGastos.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import Gasto from './Gasto'
 
 const ListadoGastos = ({gastos}) => {
@@ -5,12 +6,12 @@ const ListadoGastos = ({gastos}) => {
     <div className="listado-gastos contenedor">
       <h2>{gastos.length ? 'Gastos' : 'No Hay Gastos'}</h2>
       
-      {gastos.map( gasto => {
+      {gastos.map( gasto => (
         <Gasto
-          hey={gasto.id}
           gasto={gasto}
+          key={gasto.id}
         />
-      })}
+      ))}
     </div>
   )
 }
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -3,3 +3,14 @@ export const generarId = () => {
     const fecha = Date.now().toString(36)
     return random + fecha
 }
+
+export const formatearFecha = fecha => {
+    const fecha_ = new Date(fecha)
+    const opciones = {
+        year: "numeric",
+        month: 'long',
+        day: '2-digit',
+    }
+
+    return fecha_.toLocaleDateString('es-ES', opciones)
+}
